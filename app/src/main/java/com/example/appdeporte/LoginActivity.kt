616d7a4b7ef62814e4a75e85128bf8827ca97bed
kikiymini <caramayo@fi.uba.ar>
2023-03-16 12:33:42 -0300

    Inicio de sesion google

--- app/src/main/java/com/example/appdeporte/LoginActivity.kt
+++ app/src/main/java/com/example/appdeporte/LoginActivity.kt
@@ -1,18 +1,29 @@
 package com.example.appdeporte
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.TextUtils
 import android.view.View
+import android.widget.CheckBox
 import android.widget.EditText
 import android.widget.LinearLayout
+import android.widget.TextView
+import android.widget.Toast
+import androidx.core.content.ContextCompat
+import androidx.core.widget.doOnTextChanged
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.ktx.Firebase
+import java.text.SimpleDateFormat
+import java.util.*
 import kotlin.properties.Delegates
 
 class LoginActivity : AppCompatActivity() {
 
     companion object{
-        lateinit var useremail: String
-        lateinit var providerSesion: String
+        var useremail: String? = null
+        var providerSesion: String? = null
     }
 
     private var email by Delegates.notNull<String>()
@@ -33,5 +44,133 @@ class LoginActivity : AppCompatActivity() {
         etEmail = findViewById(R.id.etEmail)
         etPassword = findViewById(R.id.etPassword)
         mAuth = FirebaseAuth.getInstance()
+
+        manageButtonLogin()
+        etEmail.doOnTextChanged{ text, start, before, count -> manageButtonLogin()}
+        etPassword.doOnTextChanged{ text, start, before, count -> manageButtonLogin()}
     }
+
+    private fun manageButtonLogin() {
+        var tvLogin = findViewById<TextView>(R.id.tvLogin)
+        email = etEmail.text.toString()
+        password = etPassword.text.toString()
+
+        if (TextUtils.isEmpty(password) || !ValidateEmail.isEmail(email)){
+            tvLogin.setBackgroundColor(ContextCompat.getColor(this,R.color.gray))
+            tvLogin.isEnabled = false
+        }
+        else{
+            tvLogin.setBackgroundColor(ContextCompat.getColor(this,R.color.green))
+            tvLogin.isEnabled = true
+
+        }
+    }
+
+    public override fun onStart() {
+        super.onStart()
+
+        val currentUser = FirebaseAuth.getInstance().currentUser
+        if (currentUser != null){
+            goHome(currentUser.email.toString(), currentUser.providerId)
+        }
+    }
+
+    override fun onBackPressed() {
+        super.onBackPressed()
+        val startMain = Intent(Intent.ACTION_MAIN)
+        startMain.addCategory(Intent.CATEGORY_HOME)
+        startMain.flags = Intent.FLAG_ACTIVITY_NEW_TASK
+        startActivity(startMain)
+    }
+
+    fun login(view: View) {
+        loginUser()
+    }
+
+    private fun loginUser() {
+        email = etEmail.text.toString()
+        password = etPassword.text.toString()
+
+        // nos dice si el inicio fue exitoso
+        mAuth.signInWithEmailAndPassword(email,password)
+            .addOnCompleteListener(this){ task ->
+                if (task.isSuccessful) goHome(email,"email")
+                else{
+                    if (lyTerms.visibility == View.INVISIBLE) lyTerms.visibility = View.VISIBLE
+                    else{
+                        var cbAcept = findViewById<CheckBox>(R.id.cbAcept)
+                        if (cbAcept.isChecked) register()
+                    }
+                }
+            }
+    }
+
+    private fun register() {
+        email = etEmail.text.toString()
+        password = etPassword.text.toString()
+
+        FirebaseAuth.getInstance().createUserWithEmailAndPassword(email,password)
+            .addOnCompleteListener { task ->
+                if (task.isSuccessful){
+                    var dateRegister = SimpleDateFormat("dd/MM/yyyy").format(Date())
+                    var dbRegister = FirebaseFirestore.getInstance()
+                    dbRegister.collection("users").document(email).set(hashMapOf(
+                        "user" to useremail,
+                        "dataRegister" to dateRegister
+                    ))
+                    goHome(email,"email")
+                }
+                else Toast.makeText(this,"Error, algo salió mal", Toast.LENGTH_SHORT).show()
+            }
+    }
+
+    private fun goHome(email: String, provider: String) {
+
+        useremail = email
+        providerSesion = provider
+
+        val intent = Intent(this, MainActivity::class.java)
+        startActivity(intent)
+    }
+
+    fun forgotPassword(view: View) {
+       // startActivity(Intent(this, ForgotPasswordActivity::class.java))
+        resetPassword()
+    }
+
+    private fun resetPassword() {
+        var e = etEmail.text.toString()
+
+        if (!TextUtils.isEmpty(e)) {
+            mAuth.sendPasswordResetEmail(e)
+                .addOnCompleteListener { task ->
+                    if (task.isSuccessful) Toast.makeText(
+                        this,
+                        "Email enviado a $e",
+                        Toast.LENGTH_SHORT
+                    ).show()
+                    else Toast.makeText(
+                        this,
+                        "No se encontró el usuario con este correo",
+                        Toast.LENGTH_SHORT
+                    ).show()
+                }
+        }else{
+            Toast.makeText(this,"Indica un email",Toast.LENGTH_SHORT).show()
+        }
+   }
+
+    fun goTerms(view: View) {
+        startActivity(Intent(this, TermsActivity::class.java))
+    }
+
+    fun callSignInGoogle(view: View) {
+        signInGoogle()
+    }
+
+    private fun signInGoogle() {
+
+    }
+
+
 }
--- app/src/main/java/com/example/appdeporte/MainActivity.kt
+++ app/src/main/java/com/example/appdeporte/MainActivity.kt
@@ -1,11 +1,24 @@
 package com.example.appdeporte
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import com.google.firebase.auth.FirebaseAuth
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    fun logout(view: View) {
+        signOut()
+    }
+
+    private fun signOut() {
+        FirebaseAuth.getInstance().signOut()
+        startActivity(Intent(this,LoginActivity::class.java))
     }
 }
